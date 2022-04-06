69abcd7b0e45899699633a1bbd82c96490269948
orlando-messias <programadorlando@gmail.com>
2022-04-06 10:11:40 -0300

    refact: refatora conexao banco de dados com .env

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,11 @@
 import sql from 'mssql';
+import 'dotenv/config';
 
 const config = {
-  user: 'usrKestraa',
-  password: '78K&57$tr@Sv41mx',
-  server: 'SRVDBSQL',
-  database: 'DBKestraa',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
   options: {
     encrypt: true,
     trustServerCertificate: true,
